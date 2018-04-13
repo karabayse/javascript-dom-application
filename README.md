9ff9f76ab31ecfc2aa9578c7f87f7b304c67edee
Kara Bayse <karabayse@gmail.com>
2018-04-13 11:44:19 -0500

    functionality to delete and filter items

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![JavaScript DOM Application](javascript-dom-application-view.png)
+
 ## JavaScript DOM Application
 
 Uses vanilla JavaScript to create a simple list with the option to delete
--- client.js
+++ client.js
@@ -1,3 +1,4 @@
+// VARIABLES
 // We want to add an item, so we will need a submit event
 // We know that the form has an id of "addForm"
 // Put the form into a variable
@@ -7,12 +8,26 @@ let form = document.getElementById('addForm');
 // Put the list items into a variable
 let itemList = document.getElementById('items');  // could use 'querySelector'
 
+// Variable to filter items; Search Items input has an id of "filter"
+let filter = document.getElementById('filter');
+
+
+// EVENT LISTENERS
+// Filter Items Event Listener
+filter.addEventListener('keyup', filterItems);
+
+// Delete Items
+// Take the 'itemList' variable above and add an event listener
+itemList.addEventListener('click', removeItem);
+
 // Form submit event
 // Take the form variable from above, add an event listener to submit,
 // then run the function addItem
 form.addEventListener('submit', addItem);
 
-// Function addItem
+
+// FUNCTIONS
+// Add Items Function
 // Pass in event parameter
 function addItem(e) {
   // Prevent initial behavior
@@ -39,4 +54,46 @@ function addItem(e) {
   li.appendChild(deleteBtn);
   // Append li to list
   itemList.appendChild(li);
+}
+
+
+// Delete Items Function
+function removeItem(e) {
+  console.log('in removeItem');
+  // if what we're clicking contains a class of "delete"
+  if(e.target.classList.contains('delete')) {
+    console.log('delete');
+    if(confirm('Are You Sure?')) {
+      // We are clicking the button, and we want the parent element which is "li"
+      let li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+
+// Filter Items function
+function filterItems(e) {
+  console.log('in filterItems');
+  // Get Filter Text being typed in and convert it to lowercase
+  let text = e.target.value.toLowerCase();
+  // Grab all lis within itemList
+  let items = itemList.getElementsByTagName('li');
+  console.log(items);
+  // Convert HTMLCollection into an array
+  // Use 'forEach()' to loop through
+  Array.from(items).forEach(function(item) {
+    let itemName = item.firstChild.textContent;
+    console.log(itemName);
+    // Compare item name to search box text
+    // Converted text items to lowercase, therefore need to include in our comparison
+    // If it is not a match, it will equal -1
+    if(itemName.toLowerCase().indexOf(text) != -1) {
+      // If true, display in block form
+      item.style.display = 'block';
+    } else {
+      // If false, do not display
+      item.style.display = 'none';
+    }
+  });
 }
